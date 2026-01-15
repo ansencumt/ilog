6436ca3569c20fe10695480307b9ff88b00e4f29
巴朗鱼 <ansenwork@live.cn>
2026-01-15 15:21:48 +0800

    增加JSON日志

---
+++ logger/json.go
@@ -0,0 +1,33 @@
+package logger
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type IJson interface {
+	JSON(obj any) string
+}
+
+type defaultJson struct{}
+
+func (d *defaultJson) JSON(obj any) string {
+	if obj == nil {
+		return "null"
+	}
+
+	// 快路径：最常见成功情况
+	b, err := json.Marshal(obj)
+	if err == nil {
+		return string(b)
+	}
+
+	// 失败兜底：Go 原生打印
+	return fmt.Sprintf("%+v", obj)
+}
+
+var djsoner IJson = &defaultJson{}
+
+func JSON(obj any) string {
+	return djsoner.JSON(obj)
+}
